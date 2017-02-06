ad813a4eea8915e0154e8f619429b383dcae579a
timo terpstra <timoski@outlook.com>
2017-02-06 16:22:45 +0100

    timo-aanmaak printen

---
+++ TextAdventureCS/Printe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextAdventureCS
+{
+    class Printe
+    {
+        private int corXNull;
+        private int corYNull;
+        public Printe()
+        {
+            this.corXNull = 0;
+            this.corYNull = 0;
+        }
+        public void PrintMessage(int corX, int corY, string message)
+        {
+            Console.SetCursorPosition(corX, corY);
+            Console.WriteLine(message);
+            Console.SetCursorPosition(corXNull, corYNull);
+
+        }
+    }
+}

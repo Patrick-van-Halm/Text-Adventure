2a031e4057ff0b1edb38b663aa1431f31e0ae31c
Patrick-van-Halm <pvanhalm00@gmail.com>
2017-02-06 16:38:49 +0100

    Patrick - Timer.CS Klaar
    Moet alleen aangepast worden als we het echt gaan gebruiken voor de
    Console.WriteLine enzo...

---
+++ TextAdventureCS/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TextAdventureCS
+{
+    class Timer
+    {
+        private int minutes;
+
+        public Timer(int minutes = 1)
+        {
+            this.minutes = minutes;
+        }
+
+        public void Countdown()
+        {
+            for (; minutes > -1; minutes--)
+            {
+                for (int i = 59; i > -1; i--)
+                {
+                    Console.Clear();
+                    Console.WriteLine("{0} Minutes {1} Seconds", minutes, i);
+                    Thread.Sleep(200);
+                }
+            }
+        }
+    }
+}

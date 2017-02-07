8fa761dc051f46c1c9a174a6f72dcd557dcfc041
Patrick-van-Halm <pvanhalm00@gmail.com>
2017-02-07 15:44:08 +0100

    Locations - Patrick

---
+++ TextAdventureCS/Locations/Hospital.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextAdventureCS.Locations
+{
+    class Hospital : Location
+    {
+        public Hospital(string name)
+            : base(name)
+        {
+        }
+
+        public override void Description()
+        {
+            Console.WriteLine("You are in the hospital.");
+        }
+    }
+}
---
+++ TextAdventureCS/Locations/Station.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextAdventureCS.Locations
+{
+    class Station : Location
+    {
+        public Station(string name)
+            : base(name)
+        {
+        }
+
+        public override void Description()
+        {
+            Console.WriteLine("You are standing in the station.");
+        }
+    }
+}

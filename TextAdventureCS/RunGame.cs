3294e3c7a5255f05ab34bce600b7693a384d06ac
timo terpstra <timoski@outlook.com>
2017-02-08 20:21:51 +0100

    update

---
+++ TextAdventureCS/RunGame.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextAdventureCS
+{
+    class RunGame
+    {
+        public RunGame()
+        {
+            // welcoms word
+
+            // creates patrick santi timo
+
+            // start menu
+
+            // animatie met de uitleg uit santis choish systeem
+
+            // ons huis
+            LoadMap map = new LoadMap();
+
+            // inladen alles
+
+            // loop en kies systeem hertekent als nodig
+
+            // lopen met w,a,s,d
+
+            // accepteren met spacie
+
+            // cencellen met backspace
+
+            // als je een vraag word gesteld heb je y/n
+
+            // esc om er uit te komen
+
+            // met pop-up
+            // met opsla button
+
+            // knop om op te slaan
+
+            // l voor legenda
+
+            // m voor knoppen menu
+
+            // n voor op nieuw
+            // met pop-up
+
+            // als ecs gebruikt is doei
+            // als je dood gaat dan staat er game over.
+        }
+    }
+}

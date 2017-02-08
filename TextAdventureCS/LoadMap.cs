649ac1177c7a72d415c3e8ce9368bb86e43e0708
timo terpstra <timoski@outlook.com>
2017-02-08 14:28:10 +0100

    timo update

---
+++ TextAdventureCS/LoadMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TextAdventureCS
+{
+    class LoadMap
+    {
+        private string pathCity;
+        public LoadMap()
+        {
+            this.pathCity = @"";
+        }
+        public void LoadCity()
+        {
+
+        }
+    }
+}
